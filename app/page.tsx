3e2bbebd5cf20b71cc37141ef1d306354802d409
David Lee <Dai180293@gmail.com>
2024-12-29 22:57:25 +0000

    added root page hero section content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
     return (
-        <section className="px-2">
-            <h1>Home page</h1>
+        <section className="minHeight">
+            <article className="flex flex-col justify-center items-center gap-6">
+                <h1 className="text-4xl sm:text-5xl md:text-6xl font-light font-serif text-center">
+                    to be named
+                </h1>
+                <p className="max-w-2xl text-center">
+                    {" "}
+                    An all-in-one application offering a diverse set of tools designed to meet the
+                    needs of everyone, making tasks easier, more efficient, and enjoyable.
+                </p>
+            </article>
         </section>
     );
 }
