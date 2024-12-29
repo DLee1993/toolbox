431c227e5a11bbb7e20a029f401a734958af79c5
David Lee <Dai180293@gmail.com>
2024-12-29 22:41:42 +0000

    added project link to github

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
 import { SidebarTrigger } from "./ui/sidebar";
 
 export default function Header() {
     return (
-        <header className="py-[8px] px-2 md:hidden">
-            <SidebarTrigger />
+        <header className="px-2 flex justify-between md:justify-end items-center">
+            <SidebarTrigger className="md:hidden" />
+            <Button variant="link">
+                <Link href="https://github.com" target="_blank">
+                    Project repo
+                </Link>
+            </Button>
         </header>
     );
 }
