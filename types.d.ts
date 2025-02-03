dd6c7056138e6712adfe3db308f8e26262d47dff
David Lee <Dai180293@gmail.com>
2025-02-03 08:23:04 +0000

    added types for the following pages [linkInBio, Notepad]

---
+++ types.d.ts
@@ -0,0 +1,24 @@
+type LinkInBioValues = {
+    name: string;
+    email: string;
+    description: string;
+    photo: string;
+    portfolio: string;
+    facebook: string;
+    twitter: string;
+    linkedin: string;
+    instagram: string;
+    github: string;
+    telegram: string;
+    youtube: string;
+    whatsApp: string;
+    [key: string]: string;
+};
+
+type NotepadValues = {
+    title?: string;
+    content?: string;
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+}
