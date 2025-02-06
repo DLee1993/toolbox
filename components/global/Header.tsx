6ce889a4101ffc2e1428d06a8c6693d4e46ae473
David Lee <Dai180293@gmail.com>
2025-02-06 08:59:57 +0000

    reduced paddding on header and increased width & height of the menu trigger

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -2,8 +2,8 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Header() {
     return (
-        <header className="p-5 md:hidden">
-            <SidebarTrigger className="md:hidden" />
+        <header className="p-2 md:hidden">
+            <SidebarTrigger className="md:hidden w-10 h-10" />
         </header>
     );
 }
