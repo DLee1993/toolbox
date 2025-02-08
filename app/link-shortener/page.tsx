83a32b46449bb883de27fb3a6ae698df2dac335e
David Lee <Dai180293@gmail.com>
2025-02-08 00:48:15 +0000

    added template content

---
+++ app/link-shortener/page.tsx
@@ -0,0 +1,10 @@
+export default function LinkShortener() {
+    return (
+        <section className="minHeight customPadding">
+            <article>
+                <h1>Link Shortener</h1>
+                <p>Shorten your long links with our link shortener service.</p>
+            </article>
+        </section>
+    );
+}
