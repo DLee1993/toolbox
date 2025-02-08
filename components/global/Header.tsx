dd02fe57bdf4e10840098eb703284a4bb439611e
David Lee <Dai180293@gmail.com>
2025-02-08 06:16:00 +0000

    changed header to be display sticky

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -2,7 +2,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Header() {
     return (
-        <header className="p-2 md:hidden">
+        <header className="p-2 sticky top-0 md:hidden">
             <SidebarTrigger variant="outline" className="md:hidden w-10 h-10" />
         </header>
     );
