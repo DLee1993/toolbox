be0a1affa506de3a287d4bac3641827527dc6d5b
David Lee <Dai180293@gmail.com>
2025-02-12 15:04:38 +0000

    added: sidebar header and sidebar items from item list

--- components/global/app-sidebar.tsx
+++ components/global/app-sidebar.tsx
@@ -1,15 +1,123 @@
 "use client";
 
-// import { usePathname } from "next/navigation";
-// import Link from "next/link";
-// import { Home, Library, FolderGit2, Shield, ChevronDown } from "lucide-react";
-// import { items } from "./app-sidebar-item-list";
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarSeparator,
+    SidebarTrigger,
+    useSidebar,
+} from "../ui/sidebar";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { Home, Library, FolderGit2 } from "lucide-react";
+import { sidebarItems } from "./app-sidebar-item-list";
 
 export function AppSidebar() {
-    // const pathname = usePathname();
-    // const { open } = useSidebar();
+    const pathname = usePathname();
+    const { open } = useSidebar();
 
     return (
-        <p>sidebare here</p>
+        <Sidebar
+            variant="inset"
+            collapsible="icon"
+            className="border-r border-muted z-50 px-0"
+        >
+            <SidebarHeader className="overflow-hidden px-0">
+                <div className="relative hidden md:flex justify-end items-center space-x-2 px-0">
+                    <SidebarTrigger
+                        className="absolute top-1/2 -translate-y-1/2 right-1.5 w-full max-w-10 h-10 z-10 cursor-pointer hover:bg-muted"
+                        type="button"
+                        variant="ghost"
+                    />
+                    <p
+                        className={`flex flex-col min-w-52 transition-opacity duration-300 ease-linear ${
+                            !open && "opacity-0"
+                        }`}
+                    >
+                        Toolbox
+                        <span className="text-xs text-muted-foreground font-normal">
+                            A hub for essential tools.
+                        </span>
+                    </p>
+                </div>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup className="mt-2.5">
+                    <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
+                    <SidebarGroupContent
+                        className={open ? "overflow-y-auto" : "overflow-y-auto hiddenScrollbar"}
+                    >
+                        <SidebarMenu>
+                            <SidebarMenuItem>
+                                <SidebarMenuButton
+                                    asChild
+                                    tooltip="Home"
+                                    className="space-x-2 min-h-10 min-w-10 text-muted-foreground hover:text-foreground"
+                                    isActive={pathname === "/" ? true : false}
+                                >
+                                    <Link href="/">
+                                        <Home className="ml-1" />
+                                        <p className="min-w-52">Home</p>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                            <SidebarMenuItem>
+                                <SidebarMenuButton
+                                    asChild
+                                    tooltip="Request a tool"
+                                    className="space-x-2 min-h-10 min-w-10 text-muted-foreground hover:text-foreground"
+                                >
+                                    <Link href="https://github.com/DLee1993" target="_blank">
+                                        <FolderGit2 className="ml-1" />
+                                        <p className="min-w-52">Request a tool</p>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                    {sidebarItems.map((group, index) => (
+                        <section key={`Items-group: ${index}`}>
+                            <SidebarSeparator className="mt-2.5"/>
+                            <SidebarGroup className="overflow-hidden px-0">
+                                <SidebarGroupContent key={index}>
+                                    <SidebarGroupLabel className="z-0">
+                                        {group.category}
+                                    </SidebarGroupLabel>
+                                    <SidebarMenu>
+                                        {group.tools.map((tool, index) => (
+                                            <SidebarMenuItem key={index}>
+                                                <SidebarMenuButton
+                                                    asChild
+                                                    tooltip={tool.title}
+                                                    className="space-x-2 min-h-10 min-w-10 text-muted-foreground hover:text-foreground"
+                                                    isActive={pathname === tool.url ? true : false}
+                                                >
+                                                    <Link href={tool.url}>
+                                                        {"icon" in tool ? (
+                                                            <tool.icon className="ml-1" size={15} />
+                                                        ) : (
+                                                            <Library className="ml-1" />
+                                                        )}
+                                                        <p className="min-w-52">{tool.title}</p>
+                                                    </Link>
+                                                </SidebarMenuButton>
+                                            </SidebarMenuItem>
+                                        ))}
+                                    </SidebarMenu>
+                                </SidebarGroupContent>
+                            </SidebarGroup>
+                        </section>
+                    ))}
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
     );
 }
