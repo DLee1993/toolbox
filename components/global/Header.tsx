055e0419deadcc9dccda2200f57156735250a85b
David Lee <Dai180293@gmail.com>
2025-02-12 14:59:34 +0000

    updated: removed custom class, added: custom padding and height

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -4,8 +4,8 @@ import ChangeTheme from "./ThemeToggle";
 
 export default function Header() {
     return (
-        <header className="customPadding sticky top-0 z-10 flex justify-between items-center bg-background">
-            <div className="flex items-center gap-4">
+        <header className="sticky top-0 z-10 min-h-20 flex justify-between items-center bg-background border-b-2 border-muted px-2 sm:px-5 md:px-7 lg:px-10">
+            <div className="flex items-center gap-2">
                 <SidebarTrigger variant="ghost" className="md:hidden w-10 h-10" />
                 <BreadCrumbNav />
             </div>
