f5f77c01079ff4ec62f06f449557c2c2d8849d60
David Lee <Dai180293@gmail.com>
2025-02-12 00:44:25 +0000

    updated: component now stays visible on all viewports, components within the header are removed/added based on screen size

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -1,9 +1,15 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import BreadCrumbNav from "./BreadCrumbNav";
+import ChangeTheme from "./ThemeToggle";
 
 export default function Header() {
     return (
-        <header className="p-2 sticky top-0 md:hidden">
-            <SidebarTrigger variant="outline" className="md:hidden w-10 h-10" />
+        <header className="customPadding sticky top-0 z-10 flex justify-between items-center bg-background">
+            <div className="flex items-center gap-4">
+                <SidebarTrigger variant="ghost" className="md:hidden w-10 h-10" />
+                <BreadCrumbNav />
+            </div>
+            <ChangeTheme />
         </header>
     );
 }
