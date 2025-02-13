fb89e5dbd19fb5bde72e9b1a99f1e18a67ae8bd3
David Lee <Dai180293@gmail.com>
2025-02-13 14:51:54 +0000

    added: screen size based visibility to the div wrapper of the breadcrumb components

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -10,15 +10,25 @@ export default function Header() {
         <header className="sticky top-0 z-10 min-h-14 flex justify-between items-center bg-background border-b-2 border-muted px-2 sm:px-5 md:px-7">
             <div className="flex items-center gap-2">
                 <SidebarTrigger variant="ghost" className="md:hidden w-10 h-10" />
-                <BreadCrumbNav />
+                <div className="hidden min-[600px]:block">
+                    <BreadCrumbNav />
+                </div>
             </div>
             <div className="flex gap-4 w-fit">
-                <Link href="https://github.com/DLee1993" target="_blank" className="hidden min-[600px]:block">
+                <Link
+                    href="https://github.com/DLee1993"
+                    target="_blank"
+                    className="hidden min-[600px]:block"
+                >
                     <Button variant="outline">
                         <StarIcon /> support us
                     </Button>
                 </Link>
-                <Link href="https://github.com/DLee1993" target="_blank" className="hidden min-[600px]:block">
+                <Link
+                    href="https://github.com/DLee1993"
+                    target="_blank"
+                    className="hidden min-[600px]:block"
+                >
                     <Button variant="outline">Request a tool</Button>
                 </Link>
                 <ChangeTheme />
