2c639f832ccaa8b086f519a77963b3ad407139d3
David Lee <Dai180293@gmail.com>
2025-02-13 14:09:45 +0000

    removed: padding for large viewports >

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -7,7 +7,7 @@ import { StarIcon } from "lucide-react";
 
 export default function Header() {
     return (
-        <header className="sticky top-0 z-10 min-h-14 flex justify-between items-center bg-background border-b-2 border-muted px-2 sm:px-5 md:px-7 lg:px-10">
+        <header className="sticky top-0 z-10 min-h-14 flex justify-between items-center bg-background border-b-2 border-muted px-2 sm:px-5 md:px-7">
             <div className="flex items-center gap-2">
                 <SidebarTrigger variant="ghost" className="md:hidden w-10 h-10" />
                 <BreadCrumbNav />
