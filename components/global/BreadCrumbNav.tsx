0954acdff563ed99258a2e16de7bd2dad07d29c2
David Lee <Dai180293@gmail.com>
2025-02-21 19:17:06 +0000

    added: a dropdown menu to the breadcrumb nav, this allows users to quickly switch between tools

--- components/global/BreadCrumbNav.tsx
+++ components/global/BreadCrumbNav.tsx
@@ -1,17 +1,28 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import { sidebarItems } from "./app-sidebar-item-list";
 import {
     Breadcrumb,
+    BreadcrumbEllipsis,
     BreadcrumbItem,
     BreadcrumbLink,
     BreadcrumbList,
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { usePathname } from "next/navigation";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function BreadCrumbNav() {
     const path = usePathname();
+    const tools = sidebarItems
+        .filter((sidebarItems) => sidebarItems.category === "Tools")[0]
+        .items.filter((item) => item.url !== path);
 
     return (
         <Breadcrumb>
@@ -20,12 +31,28 @@ export default function BreadCrumbNav() {
                     <BreadcrumbItem>
                         <BreadcrumbLink href="/">Home</BreadcrumbLink>
                     </BreadcrumbItem>
-                    <>
-                        <BreadcrumbSeparator />
-                        <BreadcrumbItem>
-                            <BreadcrumbPage>{path.slice(1)}</BreadcrumbPage>
-                        </BreadcrumbItem>
-                    </>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger className="flex items-center gap-1">
+                                <BreadcrumbEllipsis className="h-4 w-4" />
+                                <span className="sr-only">Toggle menu</span>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="start">
+                                {tools.map((item, i) => (
+                                    <DropdownMenuItem key={i}>
+                                        <BreadcrumbLink href={item.url}>
+                                            {item.title}
+                                        </BreadcrumbLink>
+                                    </DropdownMenuItem>
+                                ))}
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>{path.slice(1)}</BreadcrumbPage>
+                    </BreadcrumbItem>
                 </BreadcrumbList>
             )}
         </Breadcrumb>
