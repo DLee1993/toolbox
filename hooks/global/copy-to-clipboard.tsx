aae85307edc794bf296c9aff03a4e23fa94c28ff
David Lee <Dai180293@gmail.com>
2025-02-22 12:07:26 +0000

    added an additional function step to copy to clipboard to allow for an automatic toast to pop when successfull or failed to copy to clipboard

--- hooks/global/copy-to-clipboard.tsx
+++ hooks/global/copy-to-clipboard.tsx
@@ -1,10 +1,25 @@
-export const WriteToClipboard = async ({ input }: { input: string }) => {
+import { NotifyUser } from "./NotifyUser";
+
+const writeToClipboard = async (input: string) => {
     try {
         await navigator.clipboard.writeText(input);
         return true;
     } catch (error) {
         console.log(error);
-        new Error("Unable to copy to clipboard, please try again");
-        return false;
+        return error;
+    }
+};
+
+export const Copy = async ({ input }: { input: string }) => {
+    if (input.length === 0) {
+        return NotifyUser("Error", "Nothing to copy");
+    } else {
+        const success = await writeToClipboard(input);
+
+        if (success) {
+            NotifyUser("Success", "Copied to clipboard");
+        } else {
+            NotifyUser("Error", "Failed to copy to clipboard");
+        }
     }
 };
