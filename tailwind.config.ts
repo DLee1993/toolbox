b03d7b384312f2163a09fb68af22e696db2216c7
David Lee <Dai180293@gmail.com>
2025-02-25 07:16:29 +0000

    removed: sidebar custom styling variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ export default {
             fontSize: {
                 sm: "var(--fs-sm)",
                 base: "var(--fs-base)",
-				md: "var(--fs-md)",
+                md: "var(--fs-md)",
                 lg: "var(--fs-lg)",
                 xl: "var(--fs-xl)",
             },
@@ -56,16 +56,6 @@ export default {
                     "3": "hsl(var(--chart-3))",
                     "4": "hsl(var(--chart-4))",
                     "5": "hsl(var(--chart-5))",
-                },
-                sidebar: {
-                    DEFAULT: "hsl(var(--sidebar-background))",
-                    foreground: "hsl(var(--sidebar-foreground))",
-                    primary: "hsl(var(--sidebar-primary))",
-                    "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
-                    accent: "hsl(var(--sidebar-accent))",
-                    "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
-                    border: "hsl(var(--sidebar-border))",
-                    ring: "hsl(var(--sidebar-ring))",
                 },
             },
             borderRadius: {
