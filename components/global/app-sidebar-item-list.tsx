6d3d857d850cfe2fc143dff330c5220ac24fc0b3
David Lee <Dai180293@gmail.com>
2025-02-27 21:23:53 +0000

    updated: seperated out the root items (home etc) from the collapsible menu items, both now are in a seperate array

--- components/global/app-sidebar-item-list.tsx
+++ components/global/app-sidebar-item-list.tsx
@@ -5,37 +5,47 @@ import {
     Notebook,
     Network,
     Unlink,
+    SquareTerminal,
     Home,
     FolderGit2,
     CoffeeIcon,
+    MegaphoneIcon,
 } from "lucide-react";
 
-export const sidebarItems = [
+//! DO NOT REMOVE
+export const sidebarRootItems = [
     {
-        category: "Dashboard",
-        items: [
-            {
-                title: "Home",
-                url: "/",
-                icon: Home,
-                target: false,
-            },
-            {
-                title: "Request a tool",
-                url: "https://github.com/DLee1993",
-                icon: FolderGit2,
-                target: true,
-            },
-            {
-                title: "Buy us a coffee",
-                url: "https://github.com/DLee1993",
-                icon: CoffeeIcon,
-                target: true,
-            },
-        ],
+        title: "Home",
+        url: "/",
+        icon: Home,
+        target: false,
     },
     {
-        category: "Tools",
+        title: "Request a tool",
+        url: "https://github.com/DLee1993",
+        icon: FolderGit2,
+        target: true,
+    },
+    {
+        title: "Buy us a coffee",
+        url: "https://github.com/DLee1993",
+        icon: CoffeeIcon,
+        target: true,
+    },
+    {
+        title: "Advertisements",
+        url: "https://www.google.com",
+        icon: MegaphoneIcon,
+        target: true,
+    },
+];
+
+//? - ADD NEW ADDITIONS TO APP HERE
+
+export const sidebarSubItems = [
+    {
+        title: "Tools",
+        icon: SquareTerminal,
         items: [
             {
                 title: "Credentials",
