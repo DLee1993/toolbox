52bfa6561c93a284258992190974852371d207a0
David Lee <Dai180293@gmail.com>
2025-03-05 06:37:09 +0000

    added: mobile menu trigger

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -1,12 +1,16 @@
 import BreadCrumbNav from "@/components/global/navigation/BreadCrumbNav";
 import ChangeTheme from "@/components/global/ThemeToggle";
 import { Button } from "@/components/ui/button";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="sticky top-0 z-10 min-h-14 flex justify-between items-center bg-background border border-border px-6">
-            <BreadCrumbNav />
+        <header className="sticky top-0 z-10 min-h-14 flex justify-between items-center bg-background border border-border px-2 sm:px-4 md:px-6">
+            <div className="flex items-center gap-2">
+                <SidebarTrigger variant="ghost" className="w-10 h-10 md:hidden" />
+                <BreadCrumbNav />
+            </div>
             <div className="flex items-center gap-2 w-fit">
                 <div className="hidden md:block">
                     <Link href="/advertisements">
