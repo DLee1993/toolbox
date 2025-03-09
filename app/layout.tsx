5f6bef33b86b1d7061ecd5790422e81b74a7b8e1
David Lee <Dai180293@gmail.com>
2025-03-09 18:48:59 +0000

    updated url path to metadata icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     title: "Toolbox | Tools for everyone",
     description: "A hub for essential tools",
     icons: {
-        icon: "./images/logo.svg",
+        icon: "/images/logo.svg",
     },
 };
 
