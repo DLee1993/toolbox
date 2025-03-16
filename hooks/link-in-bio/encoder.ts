92f8309a3e5dfdad0a889ea69f7023b04fd76b3d
David Lee <Dai180293@gmail.com>
2025-03-16 17:06:44 +0000

    removed encoder in favour of encryption/decrytion files

--- hooks/link-in-bio/encoder.ts
+++
@@ -1,7 +0,0 @@
-import { encode, decode } from "js-base64";
-
-export const encodeData = (userData: LinkInBioValues) => {
-    return encode(JSON.stringify(userData));
-};
-
-export const decodeData = (base64: string) => JSON.parse(decode(base64));
