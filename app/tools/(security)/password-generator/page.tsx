053384bd64acc6849ed3183bee58e1a09c4bdf46
David Lee <Dai180293@gmail.com>
2025-03-17 19:01:37 +0000

    added content to relative pages

--- app/tools/(security)/password-generator/page.tsx
+++ app/tools/(security)/password-generator/page.tsx
@@ -1,62 +1,68 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { GeneratePassword } from "@/hooks/credentials-generator/generate-credentials";
 import { Copy } from "@/hooks/global/copy-to-clipboard";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { CopyIcon, RefreshCcwIcon } from "lucide-react";
+import { Slider } from "@/components/ui/slider";
 
 export default function CredentialsGenerator() {
     const passwordInput = useRef<HTMLInputElement>(null);
+    const [length, setLength] = useState<number>(8);
 
     return (
-        <section>
-            <section className="space-y-10 py-10">
-                <article className="text-center">
-                    <h1 className="text-lg md:text-xl">Your trusted Credentials Generator.</h1>
-                    <p className="max-w-3xl mx-auto my-5">
-                        In the realm of cybersecurity, ensuring robust and secure credentials is
-                        paramount. Whether you require passwords, or pin codes, We provide a
-                        reliable solution to fortify your online presence.
-                    </p>
-                    <p className="text-sm font-bold">
-                        We recommed only using pin codes for non valuable data/applications, as pin
-                        codes are easily cracked.
-                    </p>
-                </article>
-                <section className="flex justify-between items-center gap-5 flex-col lg:flex-row">
-                    <section className="min-h-60 w-full px-2 flex flex-col justify-between items-start">
-                        <h2 className="text-base">Create a Password</h2>
-                        <Input
-                            readOnly
-                            ref={passwordInput}
-                            name="password input"
-                            placeholder="]-[vPW}~'1=>"
-                            className="!text-base text-foreground min-h-14 border-muted"
-                        ></Input>
-                        <div className="flex flex-wrap space-x-4">
-                            <Button
-                                onClick={() =>
-                                    GeneratePassword({
-                                        input: passwordInput,
-                                    })
-                                }
-                                aria-label="click to generate password"
-                            >
-                                Generate password <RefreshCcwIcon />
-                            </Button>
-                            <Button
-                                variant="secondary"
-                                onClick={() => Copy({ input: passwordInput.current?.value || "" })}
-                                aria-label="click to copy password"
-                            >
-                                Copy to clipboard <CopyIcon size={16} />
-                            </Button>
-                        </div>
-                    </section>
-                </section>
-            </section>
+        <section className="flex flex-col justify-center items-center py-10 space-y-10">
+            <article className="space-y-3 mx-auto text-center">
+                <h1 className="text-3xl font-semibold">Your trusted Password Generator.</h1>
+                <h2 className="max-w-lg">
+                    Our password generator is a reliable tool designed to create secure, random, and
+                    complex passwords.
+                </h2>
+                {/* <p className="font-semibold">All you have to do, is choose the length.</p> */}
+            </article>
+            <div className="flex gap-2">
+                <p>Length:</p>
+                <Slider
+                    defaultValue={[length]}
+                    max={30}
+                    min={1}
+                    step={1}
+                    onValueChange={(value) => setLength(value[0])}
+                    className="w-40"
+                />
+                <div>{length}</div>
+            </div>
+            <Input
+                readOnly
+                ref={passwordInput}
+                name="password input"
+                placeholder="]-[vPW}~'1=>"
+                className="w-full max-w-96 text-center text-foreground font-semibold border border-foreground"
+            ></Input>
+            <div className="flex flex-wrap gap-2 justify-center items-center mx-auto w-full max-w-96">
+                <Button
+                    onClick={() =>
+                        GeneratePassword({
+                            input: passwordInput,
+                            length,
+                        })
+                    }
+                    aria-label="click to generate password"
+                    className="clickAnim flex-1"
+                >
+                    Generate password <RefreshCcwIcon />
+                </Button>
+                <Button
+                    className="clickAnim flex-1"
+                    variant="outline"
+                    onClick={() => Copy({ input: passwordInput.current?.value || "" })}
+                    aria-label="click to copy password"
+                >
+                    Copy to clipboard <CopyIcon size={16} />
+                </Button>
+            </div>
         </section>
     );
 }
--- app/tools/(security)/pin-generator/page.tsx
+++ app/tools/(security)/pin-generator/page.tsx
@@ -6,79 +6,62 @@ import { Copy } from "@/hooks/global/copy-to-clipboard";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { CopyIcon, RefreshCcwIcon } from "lucide-react";
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select";
+import { Slider } from "@/components/ui/slider";
 
 export default function CredentialsGenerator() {
-    const pinInput = useRef<HTMLInputElement>(null);
-    const [pinLength, setPinLength] = useState<string>("5");
-    const pinLengthOptions = ["5", "6", "7", "8", "9", "10", "11", "12"];
+    const codeInput = useRef<HTMLInputElement>(null);
+    const [length, setLength] = useState<number>(4);
 
     return (
-        <section>
-            <section className="space-y-10 py-10">
-                <article className="text-center">
-                    <h1 className="text-lg md:text-xl">Your trusted Credentials Generator.</h1>
-                    <p className="max-w-3xl mx-auto my-5">
-                        In the realm of cybersecurity, ensuring robust and secure credentials is
-                        paramount. Whether you require passwords, or pin codes, We provide a
-                        reliable solution to fortify your online presence.
-                    </p>
-                    <p className="text-sm font-bold">
-                        We recommed only using pin codes for non valuable data/applications, as pin
-                        codes are easily cracked.
-                    </p>
-                </article>
-                <section className="flex justify-between items-center gap-5 flex-col lg:flex-row">
-                    <section className="min-h-60 w-full px-2 flex flex-col justify-between items-start">
-                        <div className="flex justify-between items-center w-full">
-                            <h2 className="text-base">Create a pin</h2>
-                            <Select onValueChange={setPinLength} name="pin length">
-                                <SelectTrigger
-                                    className="w-32 h-10 border shadow-none"
-                                    aria-label="select pin code length"
-                                >
-                                    <SelectValue placeholder="Code length" />
-                                </SelectTrigger>
-                                <SelectContent className="max-h-48">
-                                    {pinLengthOptions.map((option, index) => (
-                                        <SelectItem key={index} value={option}>
-                                            {option}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                        </div>
-                        <Input
-                            readOnly
-                            ref={pinInput}
-                            name="pin input"
-                            placeholder="140312"
-                            className="!text-base text-foreground min-h-14 border-muted"
-                        ></Input>
-                        <div className="flex flex-wrap space-x-4">
-                            <Button
-                                onClick={() => GenerateCode({ input: pinInput, length: pinLength })}
-                                aria-label="click to generate pin code"
-                            >
-                                Generate pin <RefreshCcwIcon />
-                            </Button>
-                            <Button
-                                variant="secondary"
-                                onClick={() => Copy({ input: pinInput.current?.value || "" })}
-                                aria-label="click to copy pin"
-                            >
-                                Copy to clipboard <CopyIcon size={16} />
-                            </Button>
-                        </div>
-                    </section>
-                </section>
-            </section>
+        <section className="flex flex-col justify-center items-center py-10 space-y-10">
+            <article className="space-y-3 mx-auto text-center">
+                <h1 className="text-3xl font-semibold">Your trusted Code Generator.</h1>
+                <h2 className="max-w-lg">
+                    Our code generator is a reliable tool designed to create random codes.
+                </h2>
+                {/* <p className="font-semibold">All you have to do, is choose the length.</p> */}
+            </article>
+            <div className="flex gap-2">
+                <p>Length:</p>
+                <Slider
+                    defaultValue={[length]}
+                    max={10}
+                    min={1}
+                    step={1}
+                    onValueChange={(value) => setLength(value[0])}
+                    className="w-40"
+                />
+                <div>{length}</div>
+            </div>
+            <Input
+                readOnly
+                ref={codeInput}
+                name="code input"
+                placeholder="49302"
+                className="w-full max-w-96 text-center text-foreground font-semibold border border-foreground"
+            ></Input>
+            <div className="flex flex-wrap gap-2 justify-center items-center mx-auto w-full max-w-96">
+                <Button
+                    onClick={() =>
+                        GenerateCode({
+                            input: codeInput,
+                            length,
+                        })
+                    }
+                    aria-label="click to generate code code"
+                    className="clickAnim flex-1"
+                >
+                    Generate code <RefreshCcwIcon />
+                </Button>
+                <Button
+                    className="clickAnim flex-1"
+                    variant="outline"
+                    onClick={() => Copy({ input: codeInput.current?.value || "" })}
+                    aria-label="click to copy code"
+                >
+                    Copy to clipboard <CopyIcon size={16} />
+                </Button>
+            </div>
         </section>
     );
 }
