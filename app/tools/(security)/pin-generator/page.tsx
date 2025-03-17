26117359f5523ede66438a2e3bb8a8da16682215
David Lee <Dai180293@gmail.com>
2025-03-17 14:47:04 +0000

    seperated the password and pin code generators into individual pages

--- app/tools/(security)/pin-generator/page.tsx
+++ app/tools/(security)/pin-generator/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useRef, useState } from "react";
-import { GeneratePassword, GenerateCode } from "@/hooks/credentials-generator/generate-credentials";
+import { GenerateCode } from "@/hooks/credentials-generator/generate-credentials";
+import { Copy } from "@/hooks/global/copy-to-clipboard";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { CopyIcon, RefreshCcwIcon } from "lucide-react";
-import { Copy } from "@/hooks/global/copy-to-clipboard";
 import {
     Select,
     SelectContent,
@@ -13,10 +13,8 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
-import { Separator } from "@/components/ui/separator";
 
 export default function CredentialsGenerator() {
-    const passwordInput = useRef<HTMLInputElement>(null);
     const pinInput = useRef<HTMLInputElement>(null);
     const [pinLength, setPinLength] = useState<string>("5");
     const pinLengthOptions = ["5", "6", "7", "8", "9", "10", "11", "12"];
@@ -37,36 +35,6 @@ export default function CredentialsGenerator() {
                     </p>
                 </article>
                 <section className="flex justify-between items-center gap-5 flex-col lg:flex-row">
-                    <section className="min-h-60 w-full px-2 flex flex-col justify-between items-start">
-                        <h2 className="text-base">Create a Password</h2>
-                        <Input
-                            readOnly
-                            ref={passwordInput}
-                            name="password input"
-                            placeholder="]-[vPW}~'1=>"
-                            className="!text-base text-foreground min-h-14 border-muted"
-                        ></Input>
-                        <div className="flex flex-wrap space-x-4">
-                            <Button
-                                onClick={() =>
-                                    GeneratePassword({
-                                        input: passwordInput,
-                                    })
-                                }
-                                aria-label="click to generate password"
-                            >
-                                Generate password <RefreshCcwIcon />
-                            </Button>
-                            <Button
-                                variant="secondary"
-                                onClick={() => Copy({ input: passwordInput.current?.value || "" })}
-                                aria-label="click to copy password"
-                            >
-                                Copy to clipboard <CopyIcon size={16} />
-                            </Button>
-                        </div>
-                    </section>
-                    <Separator orientation="vertical" className="lg:h-72 bg-muted" />
                     <section className="min-h-60 w-full px-2 flex flex-col justify-between items-start">
                         <div className="flex justify-between items-center w-full">
                             <h2 className="text-base">Create a pin</h2>
