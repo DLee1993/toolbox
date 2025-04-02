e3e75bff7dac94c24fbebbf92ce6c7ebf6af2ffb
David Lee <Dai180293@gmail.com>
2025-04-02 11:26:04 +0100

    removed

--- hooks/notepad/crud.tsx
+++
@@ -1,4 +0,0 @@
-export const getAllNotes = async () => {};
-export const addNote = async (note: NotepadValues) => {};
-export const updateNote = async (note: NotepadValues) => {};
-export const deleteNote = async (id: NotepadValues) => {};
