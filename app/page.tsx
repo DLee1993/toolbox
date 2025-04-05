d85851775d7624a2be3e7e76899c8905c2e1bda4
David Lee <Dai180293@gmail.com>
2025-04-05 21:29:52 +0100

    updated root page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,31 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import screenshot from "@/public/images/screenshot.webp";
 import { StarIcon } from "lucide-react";
+import { sidebarItems } from "@/components/global/navigation/app-sidebar-item-list";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 
 export default function Home() {
     return (
         <section className="py-10">
-            <article className="flex flex-col justify-center items-center w-11/12 max-w-xl mx-auto space-y-10">
-                <div className="text-center">
-                    <sup className="text-primary text-sm">Welcome to</sup>
-                    <h1 className="text-4xl font-semibold">Your digital Toolbox.</h1>
-                </div>
-                <p className="mt-4 text-pretty max-w-lg text-center">
-                    A platform offering free tools that empower people to navigate the digital world
-                    with ease, efficiency, and security.
-                </p>
-
+            <section className="flex flex-col justify-center items-center mx-auto space-y-10 py-10">
+                <article className="space-y-5">
+                    <div className="text-center">
+                        <sup className="text-muted-foreground text-sm">Welcome to</sup>
+                        <h1 className="text-2xl sm:text-3xl md:text-4xl font-semibold">Your digital Toolbox.</h1>
+                    </div>
+                    <p className="text-pretty text-center text-muted-foreground">
+                        A platform offering free web tools to simplify your daily tasks.
+                    </p>
+                </article>
                 <div className="flex gap-4 w-fit lg:mx-0">
                     <Link href="https://github.com/DLee1993/toolbox" target="_blank">
                         <Button className="border border-muted hover:text-foreground">
@@ -26,17 +33,32 @@ export default function Home() {
                         </Button>
                     </Link>
                 </div>
-            </article>
-            <figure className="my-20">
-                <Image
-                    priority
-                    alt="Product screenshot"
-                    src={screenshot}
-                    width={1000}
-                    height={1000}
-                    className="mx-auto w-11/12 max-w-3xl rounded-xl ring-1 shadow-xl ring-gray-400/10"
-                />
-            </figure>
+            </section>
+
+            <section className="mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center">
+                {sidebarItems.map((item) =>
+                    item.items.map((subItem, i) => (
+                        <Card key={i} className="w-full flex flex-col justify-between items-center">
+                            <CardHeader className="text-center space-y-2">
+                                <CardTitle className="text-xl">{subItem.title}</CardTitle>
+                                <CardDescription>{subItem.description}</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <p className="sr-only">
+                                    link to {subItem.title} web tool provided by the platform
+                                </p>
+                            </CardContent>
+                            <CardFooter className="w-full flex justify-between">
+                                <Button className="w-full p-0">
+                                    <Link href={subItem.url} className="block size-full px-4 py-2">
+                                        Open Tool
+                                    </Link>
+                                </Button>
+                            </CardFooter>
+                        </Card>
+                    ))
+                )}
+            </section>
         </section>
     );
 }
