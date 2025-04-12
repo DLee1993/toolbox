bfd3e53f3a185858f5fb971b0b2f881e41fc49c6
David Lee <Dai180293@gmail.com>
2025-04-12 22:12:50 +0100

    added notepad table to root page

--- app/tools/(productivity)/notepad/page.tsx
+++ app/tools/(productivity)/notepad/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { getAllNotes, deleteNote } from "@/lib/notepad/crud";
-import NewNote from "@/components/notepad/NewNote";
-import { Trash } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import UpdateNote from "@/components/notepad/UpdateNote";
+import { getAllNotes } from "@/lib/notepad/crud";
+import { NotepadTable } from "@/components/notepad/NotepadTable";
 
 export default function Notepad() {
-    // all notes
+    // get all notes
     const [currentNotes, setCurrentNotes] = useState<NotepadNoteValues[] | null>(null);
 
     useEffect(() => {
@@ -16,36 +13,12 @@ export default function Notepad() {
         setCurrentNotes(notes);
     }, []);
 
-    function deleteData(id: string) {
-        const updatedNotes = deleteNote(id);
-        setCurrentNotes(updatedNotes);
-    }
-
     return (
         <section className="h-screen w-full py-5">
-            <header className="place-items-end">
-                <div className="space-x-2">
-                    <NewNote setCurrentNotes={setCurrentNotes} />
-                </div>
-            </header>
-            <section className="mt-10">
-                {currentNotes
-                    ? currentNotes.map((note: NotepadNoteValues) => (
-                          <div key={note.id}>
-                              <p>{note.title}</p>
-                              <Button
-                                  variant="destructive"
-                                  size="icon"
-                                  aria-label="click to delete note"
-                                  onClick={() => deleteData(note.id)}
-                              >
-                                  <Trash size={15} />
-                              </Button>
-                              <UpdateNote setCurrentNotes={setCurrentNotes} id={note.id} />
-                          </div>
-                      ))
-                    : ""}
-            </section>
+            <NotepadTable
+                data={currentNotes ? currentNotes : []}
+                setCurrentNotes={setCurrentNotes}
+            />
         </section>
     );
 }
