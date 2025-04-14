5d10795cde5f219d026e9dab5b82407ad9c273a4
David Lee <Dai180293@gmail.com>
2025-04-14 05:35:51 +0100

    created new component for selecting a unit of measurement

---
+++ components/unit-converter/SelectUnit.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import * as React from "react";
+import { MeasurementTypes } from "@/lib/unit-conversion/convertUnits";
+import { Unit } from "convert-units";
+
+import { useMediaQuery } from "@/lib/global/use-media-query";
+import { Button } from "@/components/ui/button";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command";
+import {
+    Drawer,
+    DrawerContent,
+    DrawerDescription,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+
+type Props = {
+    type: "from" | "to";
+    selectedValue: {
+        from: Unit | "";
+        to: Unit | "";
+    };
+    setSelectedValue: React.Dispatch<
+        React.SetStateAction<{
+            from: Unit | "";
+            to: Unit | "";
+        }>
+    >;
+};
+
+export function SelectUnit({ type, selectedValue, setSelectedValue }: Props) {
+    const [open, setOpen] = React.useState(false);
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+
+    if (isDesktop) {
+        return (
+            <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                    <Button variant="outline" className="w-[150px] justify-start">
+                        {selectedValue[type] ? selectedValue[type] : `Convert ${type}`}
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] p-0" align="start">
+                    <StatusList setOpen={setOpen} setSelectedUnit={setSelectedValue} type={type} />
+                </PopoverContent>
+            </Popover>
+        );
+    }
+
+    return (
+        <Drawer open={open} onOpenChange={setOpen}>
+            <DrawerTrigger asChild>
+                <Button variant="outline" className="w-[150px] justify-start">
+                    {selectedValue[type] ? selectedValue[type] : `Convert ${type}`}
+                </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+                <div className="mt-4 border-t">
+                    <DrawerHeader>
+                        <DrawerTitle>Select a unit</DrawerTitle>
+                        <DrawerDescription>Choose the unit you want to use.</DrawerDescription>
+                    </DrawerHeader>
+                    <StatusList setOpen={setOpen} setSelectedUnit={setSelectedValue} type={type} />
+                </div>
+            </DrawerContent>
+        </Drawer>
+    );
+}
+
+function StatusList({
+    setOpen,
+    setSelectedUnit,
+    type,
+}: {
+    setOpen: (open: boolean) => void;
+    setSelectedUnit: React.Dispatch<React.SetStateAction<{ from: Unit | ""; to: Unit | "" }>>;
+    type: string;
+}) {
+    return (
+        <Command>
+            <CommandInput placeholder="Filter units..." />
+            <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                {MeasurementTypes.map((mType) => (
+                    <CommandGroup key={mType.measurement}>
+                        <h4 className="text-sm font-semibold p-2 capitalize bg-muted/50">
+                            {mType.measurement}
+                        </h4>
+                        {mType.units.map((unit, i) => (
+                            <CommandItem
+                                key={i}
+                                className="cursor-pointer ml-1"
+                                value={unit}
+                                onSelect={(value) => {
+                                    setSelectedUnit((prev) => ({ ...prev, [type]: value }));
+                                    setOpen(false);
+                                }}
+                            >
+                                {unit}
+                            </CommandItem>
+                        ))}
+                        {/* <CommandSeparator /> */}
+                    </CommandGroup>
+                ))}
+            </CommandList>
+        </Command>
+    );
+}
