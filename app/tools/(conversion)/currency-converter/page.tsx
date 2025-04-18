62b5f269e425d52a37b296e23be04ba44d065f5e
David Lee <Dai180293@gmail.com>
2025-04-18 12:37:21 +0100

    added currency converter tool

---
+++ app/tools/(conversion)/currency-converter/page.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { CircleAlert, CircleX } from "lucide-react";
+import { SelectCurrency } from "@/components/currency-converter/SelectCurrency";
+import { ConvertCurrency } from "@/lib/currency-converter/convert";
+
+export default function CurrencyConverter() {
+    const [amount, setAmount] = useState("");
+    const [selectedValue, setSelectedValue] = useState<{ from: ""; to: "" }>({
+        from: "",
+        to: "",
+    });
+    const [countryCodes, setCountryCodes] = useState<{ [key: string]: number }>({});
+    const [result, setResult] = useState<number | null>(null);
+    const [error, setError] = useState<string>("");
+
+    const ClearUnits = () => {
+        setSelectedValue({ from: "", to: "" });
+        setAmount("");
+        setResult(null);
+        setError("");
+    };
+
+    useEffect(() => {
+        async function fetchCurrency() {
+            try {
+                const exchangeRates = await fetch("https://open.er-api.com/v6/latest/GBP", {
+                    method: "GET",
+                });
+                const data = await exchangeRates.json();
+
+                setCountryCodes(data.rates);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchCurrency();
+    }, []);
+
+    return (
+        <section className="flex flex-col py-10">
+            <article className="space-y-3">
+                <h1 className="text-2xl md:text-3xl font-semibold">Currency converter.</h1>
+                <p className="max-w-lg w-11/12">
+                    This free conversion calculator converts between global currencies.
+                </p>
+            </article>
+            <p className="text-[10px] text-muted-foreground mt-2">
+                Powered by -{" "}
+                <a href="https://www.exchangerate-api.com">Rates By Exchange Rate API</a>
+            </p>
+            <section className="my-10 space-y-5">
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-sm font-semibold text-muted-foreground">From</h2>
+                    <div className="flex gap-2">
+                        <SelectCurrency
+                            type="from"
+                            selectedValue={selectedValue}
+                            setSelectedValue={setSelectedValue}
+                            countryCodes={countryCodes}
+                        />
+                        <Input
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
+                            className="max-w-52 sm:text-center"
+                            placeholder="Amount"
+                            type="number"
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-sm font-semibold text-muted-foreground">To</h2>
+                    <div className="flex gap-2">
+                        <SelectCurrency
+                            type="to"
+                            selectedValue={selectedValue}
+                            setSelectedValue={setSelectedValue}
+                            countryCodes={countryCodes}
+                        />
+                        <Input
+                            readOnly
+                            value={result !== null ? Number(result).toLocaleString() : ""}
+                            className="max-w-52 sm:text-center"
+                        />
+                    </div>
+                </div>
+                <p className="min-h-5 text-red-600 text-sm font-medium">
+                    {error && (
+                        <span className="flex items-center gap-2">
+                            <CircleAlert size={15} />
+                            {error}
+                        </span>
+                    )}
+                </p>
+            </section>
+            <div className="flex gap-5">
+                <Button
+                    variant="secondary"
+                    className="cursor-pointer text-sm"
+                    onClick={() => ClearUnits()}
+                    disabled={!selectedValue.from}
+                >
+                    Reset
+                    <CircleX />
+                </Button>
+                <Button
+                    onClick={() => {
+                        ConvertCurrency({
+                            amount: Number(amount),
+                            to: selectedValue.to,
+                            rates: countryCodes,
+                            setResult,
+                            setError,
+                        });
+                    }}
+                >
+                    Convert
+                </Button>
+            </div>
+        </section>
+    );
+}
---
+++ components/currency-converter/SelectCurrency.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import * as React from "react";
+
+import { useMediaQuery } from "@/lib/global/use-media-query";
+import { Button } from "@/components/ui/button";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command";
+import {
+    Drawer,
+    DrawerContent,
+    DrawerDescription,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { ChevronDown } from "lucide-react";
+
+type Props = {
+    type: "from" | "to";
+    selectedValue: {
+        from: "";
+        to: "";
+    };
+    countryCodes: {
+        [key: string]: number;
+    };
+    setSelectedValue: React.Dispatch<
+        React.SetStateAction<{
+            from: "";
+            to: "";
+        }>
+    >;
+};
+
+export function SelectCurrency({ type, selectedValue, setSelectedValue, countryCodes }: Props) {
+    const [open, setOpen] = React.useState(false);
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+
+    if (isDesktop) {
+        return (
+            <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                    <Button
+                        variant="outline"
+                        className="w-[150px] flex justify-between items-center"
+                    >
+                        {selectedValue[type] ? selectedValue[type] : `Select currency`}
+                        <ChevronDown className={`${open ? "rotate-180" : "rotate-0"}`} />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] h-60 p-0" align="start">
+                    <CurrencyList
+                        setOpen={setOpen}
+                        setSelectedUnit={setSelectedValue}
+                        type={type}
+                        rates={countryCodes}
+                    />
+                </PopoverContent>
+            </Popover>
+        );
+    }
+
+    return (
+        <Drawer open={open} onOpenChange={setOpen}>
+            <DrawerTrigger asChild>
+                <Button variant="outline" className="w-[150px] justify-start">
+                    {selectedValue[type] ? selectedValue[type] : `Convert ${type}`}
+                </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+                <div className="mt-4 border-t">
+                    <DrawerHeader>
+                        <DrawerTitle>Select a currency</DrawerTitle>
+                        <DrawerDescription>Choose the currency you want to use.</DrawerDescription>
+                    </DrawerHeader>
+                    <CurrencyList
+                        setOpen={setOpen}
+                        setSelectedUnit={setSelectedValue}
+                        type={type}
+                        rates={countryCodes}
+                    />
+                </div>
+            </DrawerContent>
+        </Drawer>
+    );
+}
+
+function CurrencyList({
+    setOpen,
+    setSelectedUnit,
+    type,
+    rates,
+}: {
+    setOpen: (open: boolean) => void;
+    setSelectedUnit: React.Dispatch<React.SetStateAction<{ from: ""; to: "" }>>;
+    type: string;
+    rates: {
+        [key: string]: number;
+    };
+}) {
+    return (
+        <Command>
+            <CommandInput placeholder="Filter currencies..." />
+            <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                {Object.keys(rates).flatMap((rate, i) => (
+                    <CommandGroup key={rate}>
+                        <CommandItem
+                            key={i}
+                            className="cursor-pointer ml-1"
+                            value={rate}
+                            onSelect={(value) => {
+                                setSelectedUnit((prev) => ({ ...prev, [type]: value }));
+                                setOpen(false);
+                            }}
+                        >
+                            {rate}
+                        </CommandItem>
+                    </CommandGroup>
+                ))}
+            </CommandList>
+        </Command>
+    );
+}
