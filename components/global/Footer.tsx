5bcab72d3fd13a186075d609c91dab076b720b99
David Lee <Dai180293@gmail.com>
2025-04-21 01:56:39 +0100

    added link to project repository

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,11 +1,21 @@
 import { CopyrightIcon } from "lucide-react";
+import Link from "next/link";
+import { FaSquareGithub, FaGithub, FaGithubAlt } from "react-icons/fa6";
 
 export default function Footer() {
     return (
-        <footer className="flex justify-center items-center border-t-2 border-muted">
-            <p className="flex items-center gap-1 text-muted-foreground">
+        <footer className="flex justify-between items-center border-t-2 border-muted">
+            <p className="flex items-center gap-1 text-muted-foreground text-sm">
                 Toolbox <CopyrightIcon size={15} /> 2025
             </p>
+            <ul>
+                <li>
+                    <Link href="https://github.com/DLee1993/toolbox" target="_blank">
+                        <span className="sr-only">project repository</span>
+                        <FaGithub size={20} />
+                    </Link>
+                </li>
+            </ul>
         </footer>
     );
 }
