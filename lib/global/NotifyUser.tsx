d414a7a819d0a9e81d943365e308fec39d210efe
David Lee <Dai180293@gmail.com>
2025-04-27 18:40:30 +0100

    adjusted args to add optional duration when using component

--- lib/global/NotifyUser.tsx
+++ lib/global/NotifyUser.tsx
@@ -1,5 +1,13 @@
 import { toast } from "@/lib/global/use-toast";
 
-export const NotifyUser = ({ type, message }: { type: string; message: string }) => {
-    toast({ title: type, description: message, duration: 1350 });
+export const NotifyUser = ({
+    type,
+    message,
+    duration = 1350,
+}: {
+    type: string;
+    message: string;
+    duration?: number;
+}) => {
+    toast({ title: type, description: message, duration: duration });
 };
