d1c2e1b27f51de5cd19c9ee96fb3b8af87e6aea3
David Lee <Dai180293@gmail.com>
2025-05-12 13:13:34 +0100

    added plain text type component

--- components/qr-code-generator/forms/plainText.tsx
+++ components/qr-code-generator/forms/plainText.tsx
@@ -1,5 +1,38 @@
-import { Dispatch, SetStateAction } from "react";
+import { Input } from "@/components/ui/input";
+import { Dispatch, SetStateAction, useState } from "react";
+import { GenerateQrCode } from "@/lib/qr-code-generator/generateQR";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 
 export default function PLAINTEXT({ setValue }: { setValue: Dispatch<SetStateAction<string>> }) {
-    return <div>PLAINTEXT</div>;
+    const [text, setText] = useState("");
+
+    function ProcessData({ type }: { type: string }) {
+        const qrValue = GenerateQrCode({ value: { text }, type });
+
+        setValue(qrValue as string);
+    }
+
+    return (
+        <div className="space-y-5">
+            <fieldset className="relative">
+                <Input
+                    name="text"
+                    id="text"
+                    onChange={(e) => setText(e.target.value)}
+                    className="formField peer"
+                    autoComplete="true"
+                    placeholder=" "
+                />
+                <Label
+                    htmlFor="text"
+                    className={`pointer-events-none absolute text-xs duration-300 transition-all bg-background transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 
+                    peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:text-blue-600 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1`}
+                >
+                    Add any plain text
+                </Label>
+            </fieldset>
+            <Button onClick={() => ProcessData({ type: "plainText" })}>Generate QR Code</Button>
+        </div>
+    );
 }
