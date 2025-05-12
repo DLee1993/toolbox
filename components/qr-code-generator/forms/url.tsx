a2c0ce9d933d4143dba2c29bdddbff74d6abfdeb
David Lee <Dai180293@gmail.com>
2025-05-12 13:04:27 +0100

    removed text color

--- components/qr-code-generator/forms/url.tsx
+++ components/qr-code-generator/forms/url.tsx
@@ -26,7 +26,7 @@ export default function URL({ setValue }: { setValue: Dispatch<SetStateAction<st
                 />
                 <Label
                     htmlFor="url"
-                    className={`pointer-events-none absolute text-xs text-muted-foreground duration-300 transition-all bg-background transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 
+                    className={`pointer-events-none absolute text-xs duration-300 transition-all bg-background transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 
                     peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:text-blue-600 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1`}
                 >
                     Add a url to any external sources
