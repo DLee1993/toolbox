adfd1145f3c37d9d67c2d2084ec356ddcf084e09
David Lee <Dai180293@gmail.com>
2025-06-19 16:10:47 +0100

    added ffmpeg and relative components

--- types.d.ts
+++ types.d.ts
@@ -27,3 +27,17 @@ type NotepadNoteValues = {
 type NotepadCategoryValues = {
     categoryName: string;
 };
+
+type Action = {
+    file: any;
+    file_name: string;
+    file_size: number;
+    from: string;
+    to: String | null;
+    file_type: string;
+    is_converting?: boolean;
+    is_converted?: boolean;
+    is_error?: boolean;
+    url?: any;
+    output?: any;
+};
