5e8bbc0018c52a6b67a4b1af84ed9e4880301131
David Lee <Dai180293@gmail.com>
2025-06-21 11:03:58 +0100

    updated x axis padding

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -9,9 +9,9 @@ export default function Header() {
 
     if (pathname !== "/") {
         return (
-            <header className="sticky top-0 z-50 flex justify-between items-center border-b border-border bg-background px-5">
-                <BreadCrumbNav />
+            <header className="sticky top-0 z-50 flex justify-between items-center border-b border-border bg-background px-2 sm:px-5">
                 <SidebarTrigger variant="ghost" className="w-10 h-10 md:hidden" />
+                <BreadCrumbNav />
             </header>
         );
     }
