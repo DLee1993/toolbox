2df9ec06e9c340dbc5f1c2d233dbe0f116b83b01
David Lee <Dai180293@gmail.com>
2025-06-22 16:19:23 +0100

    added responsive mobile menu button and updated header to reflect new resposive design

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { SidebarTrigger } from "@/components/ui/sidebar";
 import BreadCrumbNav from "@/components/global/navigation/BreadCrumbNav";
 
 export default function Header() {
@@ -10,7 +9,6 @@ export default function Header() {
     if (pathname !== "/") {
         return (
             <header className="sticky top-0 z-50 flex justify-between items-center border-b border-border bg-background px-2 sm:px-5">
-                <SidebarTrigger variant="ghost" className="w-10 h-10 md:hidden" />
                 <BreadCrumbNav />
             </header>
         );
