189f062f039e35365b8aae6fbc9e05b9c99da793
David Lee <Dai180293@gmail.com>
2025-09-02 09:53:26 +0100

    added mobile sidebar menu trigger for mobile menu

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -7,12 +7,10 @@ import BreadCrumbNav from "@/components/global/navigation/BreadCrumbNav";
 export default function Header() {
     const pathname = usePathname();
 
-    if (pathname !== "/") {
-        return (
-            <header className="sticky top-0 z-50 flex justify-between items-center border-b border-border bg-background px-2 sm:px-5">
-                <SidebarTrigger className="min-w-9 min-h-9 md:hidden" />
-                <BreadCrumbNav />
-            </header>
-        );
-    }
+    return (
+        <header className="sticky top-0 z-50 flex justify-between items-center border-b border-border bg-background px-2 sm:px-5">
+            <SidebarTrigger className="min-w-9 min-h-9 md:hidden" />
+            {pathname !== "/" && <BreadCrumbNav />}
+        </header>
+    );
 }
