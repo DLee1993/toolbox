0853413ac032c87aee876eaab120c4d9e3c4d228
David Lee <Dai180293@gmail.com>
2025-09-12 23:28:09 +0100

    removed dash and added a space to the slug

--- components/global/navigation/BreadCrumbNav.tsx
+++ components/global/navigation/BreadCrumbNav.tsx
@@ -66,7 +66,7 @@ export default function BreadCrumbNav() {
                     </BreadcrumbItem>
                     <BreadcrumbSeparator />
                     <BreadcrumbItem>
-                        <BreadcrumbPage className="capitalize">{slug}</BreadcrumbPage>
+                        <BreadcrumbPage className="capitalize">{slug.split("-").join(" ")}</BreadcrumbPage>
                     </BreadcrumbItem>
                 </BreadcrumbList>
             )}
