e0323e5152cae62641a5ec3ba8896484986b91a4
David Lee <Dai180293@gmail.com>
2025-09-28 01:19:13 +0100

    updated ui layout

--- app/tools/(security)/credentials-generator/page.tsx
+++ app/tools/(security)/credentials-generator/page.tsx
@@ -6,26 +6,35 @@ import Pin from "@/components/credentials-generator/Pin";
 
 export default function CredentialsGenerator() {
     return (
-        <section className="p-5">
-            <Tabs defaultValue="password" className="relative flex flex-col">
-                <TabsList className="h-10 justify-start bg-transparent gap-5">
-                    <TabsTrigger
-                        value="password"
-                        className="rounded-none bg-background w-full md:w-auto h-full px-0 data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-primary"
-                    >
-                        Password
-                    </TabsTrigger>
-                    <span>|</span>
-                    <TabsTrigger
-                        value="code"
-                        className="rounded-none bg-background w-full md:w-auto h-full px-0 data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-primary"
-                    >
-                        Pin Code
-                    </TabsTrigger>
-                </TabsList>
-                <Password />
-                <Pin />
-            </Tabs>
+        <section className="flex justify-between items-start gap-10 p-5 flex-wrap">
+            <article className="flex-1 min-w-96 space-y-5">
+                <h1 className="font-medium text-3xl sm:text-4xl">Strong. Secure. Fast.</h1>
+                <p className="text-sm">
+                    Generate secure credentials in seconds — choose between strong passwords and
+                    numeric PINs. Passwords use a full mix of letters, numbers, and special
+                    characters for maximum strength. PINs are short, digit-only codes ideal for
+                    simpler authentication.
+                </p>
+                <p className="text-sm font-semibold">
+                    All generation happens locally, and no data is stored.
+                </p>
+            </article>
+            <section className="flex-1 min-w-96 bg-foreground text-background p-5 rounded-2xl space-y-5">
+                <h2>Choose password type</h2>
+                <Tabs defaultValue="password" className="relative flex flex-col space-y-10">
+                    <TabsList className="relative h-10 justify-start gap-2 bg-background/10">
+                        <TabsTrigger value="password" className="relative z-10 w-full h-8">
+                            Password
+                        </TabsTrigger>
+                        <span>|</span>
+                        <TabsTrigger value="code" className="relative z-10 h-8 w-full">
+                            Pin Code
+                        </TabsTrigger>
+                    </TabsList>
+                    <Password />
+                    <Pin />
+                </Tabs>
+            </section>
         </section>
     );
 }
