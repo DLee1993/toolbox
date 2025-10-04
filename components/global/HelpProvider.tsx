96398d9f7579897faa2ffb7cfbdb2b8798a408da
David Lee <Dai180293@gmail.com>
2025-10-04 13:18:09 +0100

    added a report issue button and link to settings page that will contain the form to report an issue

--- components/global/HelpProvider.tsx
+++ components/global/HelpProvider.tsx
@@ -5,6 +5,7 @@ import { HelpProviderContentList } from "@/components/global/HelpProviderContent
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import { MessageCircleQuestionIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function HelpProvider() {
     const pathname = usePathname();
@@ -16,26 +17,31 @@ export default function HelpProvider() {
                     Help <MessageCircleQuestionIcon />
                 </Button>
             </PopoverTrigger>
-            <PopoverContent className="w-screen max-w-lg bg-popover/90 backdrop-blur-sm">
-                {HelpProviderContentList.filter((item) => pathname === item.title).map(
-                    (item, i) => (
-                        <article key={i} className="flex-1 space-y-10">
-                            <div className="space-y-2.5">
-                                <p className="text-sm w-11/12">{item.description}</p>
-                            </div>
-                            <ul className="list-disc list-inside space-y-1 text-sm">
-                                {item.points.map((p, i) => (
-                                    <li key={i}>
-                                        {p.text}{" "}
-                                        <span className="text-xs font-bold">{p.subText}</span>
-                                    </li>
-                                ))}
-                            </ul>
+            <PopoverContent className="w-screen max-w-lg bg-popover/90 backdrop-blur-sm space-y-5">
+                <section>
+                    {HelpProviderContentList.filter((item) => pathname === item.title).map(
+                        (item, i) => (
+                            <article key={i} className="flex-1 space-y-10">
+                                <div className="space-y-2.5">
+                                    <p className="text-sm w-11/12">{item.description}</p>
+                                </div>
+                                <ul className="list-disc list-inside space-y-1 text-sm">
+                                    {item.points.map((p, i) => (
+                                        <li key={i}>
+                                            {p.text}{" "}
+                                            <span className="text-xs font-bold">{p.subText}</span>
+                                        </li>
+                                    ))}
+                                </ul>
 
-                            <p className="text-sm font-semibold">{item.declaration}</p>
-                        </article>
-                    )
-                )}
+                                <p className="text-sm font-semibold">{item.declaration}</p>
+                            </article>
+                        )
+                    )}
+                </section>
+                <section className="flex justify-center items-center border-t border-border pt-4">
+                    <h4>Is there a problem? <Link href="/settings#report" className="underline">Report it here</Link></h4>
+                </section>
             </PopoverContent>
         </Popover>
     );
