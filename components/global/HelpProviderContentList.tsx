a6156e1e729852c6c4f8c06fcce8c87ad5c453dc
David Lee <Dai180293@gmail.com>
2025-10-04 15:14:37 +0100

    added help guide for file converter

--- components/global/HelpProviderContentList.tsx
+++ components/global/HelpProviderContentList.tsx
@@ -10,4 +10,15 @@ export const HelpProviderContentList = [
             { text: "Copy to clipboard and use where you want", subText: "" },
         ],
     },
+    {
+        title: "/tools/file-converter",
+        description:
+            "Here's a guide to using the file converter:",
+        points: [
+            { text: "Add your files", subText: "(image, video or audio)" },
+            { text: "Select the type you want convert to", subText: "" },
+            { text: "Click the convert button", subText: "" },
+            { text: "Then click either the download all or three dots to download individually", subText: "" },
+        ],
+    },
 ];
