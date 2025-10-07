7f75d5034ec26a9bfab402fbaf04ec60cd1f6ff2
David Lee <Dai180293@gmail.com>
2025-10-07 01:27:08 +0100

    added all help/guide content for all tools

--- components/global/HelpProviderContentList.tsx
+++ components/global/HelpProviderContentList.tsx
@@ -4,14 +4,14 @@ export const HelpProviderContentList = [
         description:
             "You can generate secure credentials in seconds — choose between strong passwords and numeric PINs. PINs are short, digit-only codes. Our passwords are complex, combining:",
         points: [
-            { text: "Letters", subText: "(A-Z, a-z)" },
-            { text: "Numbers", subText: "(0-9)" },
-            { text: "Symbols", subText: "(@, #, $, %, &, *)" },
+            { text: "Letters", subText: "( A-Z, a-z )" },
+            { text: "Numbers", subText: "( 0-9 )" },
+            { text: "Symbols", subText: "( @, #, $, %, &, * )" },
         ],
         guide: {
             points: [
-                { text: "Select your type", subText: "(Password or Pin)" },
-                { text: "Select the length", subText: "(password: 8-30, pin: 4-16)" },
+                { text: "Select your type", subText: "( Password or Pin )" },
+                { text: "Select the length", subText: "( password: 8-30, pin: 4-16 )" },
                 { text: "Click Generate Credential", subText: "" },
                 { text: "Copy to clipboard and use where you want", subText: "" },
             ],
@@ -22,13 +22,13 @@ export const HelpProviderContentList = [
         description:
             "A fast and easy tool for converting audio, video, and image files between popular formats. No editing, no tweaking—just clean, reliable format conversion.",
         points: [
-            { text: "Audio", subText: "(MP3, WAV, AAC, FLAC, and more)" },
-            { text: "Video", subText: "(MP4, AVI, MOV, MKV, and more)" },
-            { text: "Images", subText: "(JPG, PNG, GIF, BMP, and more)" },
+            { text: "Audio", subText: "( MP3, WAV, AAC, FLAC, and more )" },
+            { text: "Video", subText: "( MP4, AVI, MOV, MKV, and more )" },
+            { text: "Images", subText: "( JPG, PNG, GIF, BMP, and more )" },
         ],
         guide: {
             points: [
-                { text: "Add your files", subText: "(Image, Video or Audio)" },
+                { text: "Add your files", subText: "( Image, Video or Audio )" },
                 { text: "Select the type you want convert to", subText: "" },
                 { text: "Click the convert button", subText: "" },
                 {
@@ -43,18 +43,18 @@ export const HelpProviderContentList = [
         description:
             "Convert Currency and Measurements with ease — switch between global currencies, metric and imperial units.",
         points: [
-            { text: "Currency", subText: "(160+ currencies)" },
-            { text: "Lengths", subText: "(Inches, centimeters, meters, feet, miles, and more)" },
-            { text: "Weight", subText: "(Grams, kilograms, pounds, ounces)" },
-            { text: "Temperature", subText: "(Celsius, Fahrenheit, Kelvin)" },
-            { text: "Volume", subText: "(Litres, gallons, cups, millilitres)" },
+            { text: "Currency", subText: "( 160+ currencies )" },
+            { text: "Lengths", subText: "( Inches, centimeters, meters, feet, miles, and more )" },
+            { text: "Weight", subText: "( Grams, kilograms, pounds, ounces )" },
+            { text: "Temperature", subText: "( Celsius, Fahrenheit, Kelvin )" },
+            { text: "Volume", subText: "( Litres, gallons, cups, millilitres )" },
         ],
         guide: {
             points: [
-                { text: "Select your type", subText: "(Measurements or Currency)" },
+                { text: "Select your type", subText: "( Measurements or Currency )" },
                 {
                     text: "Select the units to convert",
-                    subText: "(mm-cm, celsius-kelvin, dollars-pounds)",
+                    subText: "( mm-cm, celsius-kelvin, dollars-pounds )",
                 },
                 { text: "Add the amount you want to convert", subText: "" },
                 { text: "Click convert", subText: "" },
@@ -64,9 +64,117 @@ export const HelpProviderContentList = [
     {
         url: "/tools/timezones",
         description:
-            "A timezone selector that enables users to view and select time across global regions. You can view the current time and timezone offset compared to your local time.",
+            "A timezone selector that enables you to view and select time across global regions. You can view the current time and timezone offset compared to your local time.",
         guide: {
             points: [{ text: "Select the timezone you want to view", subText: "" }],
+        },
+    },
+    {
+        url: "/tools/focus-timer",
+        description:
+            "Your personal focus timer, built for mindful breaks. Taking a break helps with reducing mental fatigue and encourages micro recovery keeping you focused for longer.",
+        points: [
+            { text: "Take short, medium or long breaks between sessions", subText: "" },
+            { text: "Set custom times for sessions", subText: "" },
+            { text: "Control your sessions", subText: "( start, pause, reset )" },
+        ],
+        guide: {
+            points: [
+                { text: "Select your break type", subText: "( short, medium, long )" },
+                {
+                    text: "Select if you want an alarm to alert you when the break is over",
+                    subText: "",
+                },
+                {
+                    text: "Select the duration for you session",
+                    subText: "( 5 minutes - 90 minutes )",
+                },
+                { text: "Click start and focus!", subText: "" },
+            ],
+        },
+    },
+    {
+        url: "/tools/notepad",
+        description:
+            "A digital notepad for notes, tasks and ideas, manage workloads and organise your day efficiently.",
+        points: [
+            { text: "Create, read, update and delete notes on the go", subText: "" },
+            { text: "Add categories and to be completed by dates for future tasks", subText: "" },
+            {
+                text: "Quickly search using our filter menu or search bar",
+                subText: "( start, pause, reset )",
+            },
+        ],
+        guide: {
+            points: [
+                { text: "To add a note, click 'New note' and complete the form.", subText: "" },
+                {
+                    text: "To edit a note, click the pencil icon on the right handside and update the details.",
+                    subText: "",
+                },
+                {
+                    text: "To delete a note, click the red trash can button on the right hand side.",
+                    subText: "( ! Be aware this is irreversable. ! )",
+                },
+            ],
+        },
+    },
+    {
+        url: "/tools/link-in-bio",
+        description:
+            "An experimental link in bio tool, consolidate all of your professional and personal links into one. You can add:",
+        points: [
+            { text: "Personal details", subText: "( including a photo )" },
+            { text: "Social media profiles", subText: "( X, instagram, telegram, linkedin )" },
+            { text: "Personal portfolio", subText: "( website )" },
+        ],
+        guide: {
+            points: [
+                {
+                    text: "It's simple, fill in the form fields you want to show on your page.",
+                    subText: "",
+                },
+                { text: "View the preview to see what it looks like", subText: "" },
+                {
+                    text: "Click generate link when your ready",
+                    subText: "",
+                },
+                {
+                    text: "",
+                    subText:
+                        "( WARNING - Please note that this feature is experimental and still under development. As a result, we cannot guarantee the security or confidentiality of the data processed through it. )",
+                },
+            ],
+        },
+    },
+    {
+        url: "/tools/qr-code-generator",
+        description:
+            "Generate a qr code for your personal website, business, school or just for fun. You can add the following details to your qr code.",
+        points: [
+            { text: "URL", subText: "( link to social media profile or personal website )" },
+            { text: "Email Address", subText: "Email a welcome message" },
+            { text: "SMS", subText: "( Text someone a greeting when they scan your qr code )" },
+            { text: "Plain Text", subText: "( Just say hello )" },
+            { text: "WIFI", subText: "( Generate a qr code to share wifi details with people )" },
+            {
+                text: "Location",
+                subText: "( Generate a qr code to show someone where your located )",
+            },
+            {
+                text: "Custom Design!",
+                subText: "Make your qr code stand out from the crowd with the customisation tab",
+            },
+        ],
+        guide: {
+            points: [
+                {
+                    text: "It's simple, select and fill in the form field you want to attach to the qr code.",
+                    subText: "",
+                },
+                { text: "Click 'Generate QR code' when ready", subText: "" },
+                { text: "Download and share", subText: "" },
+            ],
         },
     },
 ];
